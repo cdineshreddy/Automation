562d6f2b2178ca0f880bbc056c6efb1e68ea5c97
cdineshreddy <dineshreddy.c@hotmail.com>
2020-06-06 09:23:54 +0530

    Programs Modified

---
+++ InterviewPrograms/src/LogialCoding/Question5.java
@@ -0,0 +1,14 @@
+package LogialCoding;
+
+public class Question5 {
+	
+	public Question5() {
+		return;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("We can use empty return statement  in the constructor");
+		
+	}
+
+}
